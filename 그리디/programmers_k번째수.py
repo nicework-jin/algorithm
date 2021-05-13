a4ebcf78f423d4d9beb61fc75cd0e50f4f0dcd74
nicework-jin <dev_seongjin@naver.com>
2021-05-13 09:41:33 +0900

    Create programmers_k번째수.py

---
+++ 그리디/programmers_k번째수.py
@@ -0,0 +1,14 @@
+# 프로그래머스, k번째수
+# i-1 ~ j를 인덱싱 => k-1번 째 수  
+
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i, j, k = command
+        answer.append(sorted(array[i-1:j])[k-1])
+    return answer
+
+if __name__ == "__main__":
+    array = [1, 5, 2, 6, 3, 7, 4]
+    commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+    solution(array, commands)
