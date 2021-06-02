1a7322227f7c1c997fcb2592500331c318039b70
nicework-jin <dev_seongjin@naver.com>
2021-06-03 01:10:29 +0900

    Update backjoon_1541_잃어버린괄호.py

--- 그리디/backjoon_1541_잃어버린괄호.py
+++ 그리디/backjoon_1541_잃어버린괄호.py
@@ -1,21 +1,11 @@
 import sys
-
 INPUT = sys.stdin.readline    # 10+01-1+1
 
 
-def solution():
+def solution(exp):
     """ 주어진 식에 적절히 괄호를 쳐서 최소 값으로 계산하여 리턴한다.
     """
-    exp = INPUT().split('-')
-
-    numbers = []
-    for x1 in exp:
-        x2 = x1.split('+')
-
-        count = 0
-        for num in x2:
-            count += int(num)
-
-        numbers.append(count)
+    numbers = [sum(map(int, x1.split('+'))) for x1 in exp.split('-')]
     return numbers[0] - sum(numbers[1:])
-print(solution())
+
+print(solution(INPUT()))
