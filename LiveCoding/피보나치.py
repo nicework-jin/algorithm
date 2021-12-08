24d2fb26da3f31838bc34fdf3e7336f85f06bff7
SeongjinLee <dev_seongjin@naver.com>
2021-12-08 13:43:04 +0900

    피보나치.py

--- LiveCoding/피보나치.py
+++ LiveCoding/피보나치.py
@@ -2,7 +2,23 @@
 n번째 피보나치 수열? >>>  [1 1 2 3 5 8 13 21]
 n = 7이면, 13
 n = 8이면, 21
+
+1        a
+1        b
+2        a+b   ==> (n-2) + (n-1)
+3
+5
+8
+13
+21
 """
+
+
+def solution(n):
+    if n == 1 or n == 2:
+        return 1
+    return solution(n-1) * solution(n-2)
+
 
 
 def solution1(n):
